a32f2a10914ed6e3ff6e0a01e94e580bad4ff8e7
byyEllen <ellenruth526@gmail.com>
2025-08-07 10:10:49 -0300

    react-router-dom instalado, atualização no main.tsx e no App.tsx

--- frontend/public/src/App.tsx
+++ frontend/public/src/App.tsx
@@ -1,10 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
--- frontend/public/src/main.tsx
+++ frontend/public/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './styles/global.scss'; // se já tiver criado o Sass global
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
